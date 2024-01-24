71de7a7382906ee454a90209fd5d42f5746ad11d
Alexandr Babushkin <i@e0m.ru>
2024-01-24 17:13:16 +0300

    new file:   s9113_b.py

--- s9113.py
+++ s9113.py
@@ -4,35 +4,41 @@ import beegik_tests as beegik_tests
 
 class MultiKeyDict(UserDict):
     def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
         self._alias = dict()
+        super().__init__(self, *args, **kwargs)
 
     def alias(self, key, alias):
+        self._alias[key] = key
         self._alias[alias] = key
 
     def __getitem__(self, key):
-        if key in self:
+        if key in self.__dict__:
+            return self.__dict__[key]
+        elif key in self._alias:
             key = self._alias[key]
-        return self.data[key]
+            return self.data[key]
+        elif (key not in self._alias) and (key in self.data):
+            raise KeyError(key)
+        elif (key in self.data):
+            return self.data[key]
 
     def __setitem__(self, key, value):
-        # key = self._alias[key]
-        if '_alias' in self.__dict__:
-            key = self.__dict__['_alias'].__get__(key, key)
-        self.data[key] = value
+        if key in self.__dict__:
+            self.__dict__[key] = value
+        elif key in self.data:
+            self.data[key] = value
+        elif key in self._alias:
+            self.data[self._alias[key]] = value
+        super().__setitem__(key, value)
+
+    def __delitem__(self, key):
+        if key in self._alias:
+            self.data[key] = self.data[self._alias[key]]
+            self._alias[self._alias[key]] = key
+            del self._alias[key]
+            # del self.data[key]
 
 
 tests = beegik_tests.Test_manager(
     '13.zip', globals())
-# tests.run()
-
-multikeydict = MultiKeyDict(x=100)
-
-multikeydict.alias('x', 'z')
-del multikeydict['x']
-print(multikeydict['z'])
-
-try:
-    print(multikeydict['x'])
-except KeyError:
-    print('Ключ отстутствует')
+tests.run()
